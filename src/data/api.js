2d6a47487e6f4d42144bbf711820342a5f216a29
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2024-12-05 19:54:06 +0530

    first commit

---
+++ src/data/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  // baseURL: "http://localhost:5002",
+  baseURL:"https://cravings-backend-9kit.onrender.com",
+
+  // baseURL:"https://cravings-backend-o8sk.onrender.com",
+
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": localStorage.getItem("token")
+      ? `Bearer ${localStorage.getItem("token")}`
+      : null,
+  },
+});
+
+export default api;
