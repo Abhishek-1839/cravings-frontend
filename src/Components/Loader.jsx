d2433ccc275f46741872d4c3c478573ec7c396ac
Abhishek-1839 <abhishektomarsingh7@gmail.com>
2024-12-05 20:00:13 +0530

    lp

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-// Loader.js
-import React from 'react';
-import './Loader.css'; // Add CSS for styling
 
-const Loader = () => {
-  return (
-    <div className="loader-overlay">
-      <div className="loader"></div>
-    </div>
-  );
-};
-
-export default Loader;
-=======
 import React from 'react';
 import "./Loader.css";
 
@@ -24,4 +10,4 @@ const Loader = ()=>{
     )
 }
 export default Loader;
->>>>>>> 79f1918e08056d168c134e65166524cb7f4c56c4
+
